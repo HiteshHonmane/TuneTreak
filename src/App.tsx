e5ca49c92e4af41f66d7861a50840d174dc3ab9b
HiteshHonmane <hiteshhonmane@gmail.com>
2024-05-12 18:01:39 +0530

    style: PlaySong component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import SongCard from "./components/songCards/SongCard";
 import LongSongCard from "./components/songCards/LongSongCard";
+import PlayingSongCard from "./components/songCards/PlayingSongCard";
+import SideBar from "./components/side Bar/SideBar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +12,8 @@ function App() {
     <>
       <div className=" w-full h-screen flex items-center justify-center">
         {/* <SongCard /> */}
-        <LongSongCard/>
+        <SideBar/>
+        
       </div>
     </>
   );
---
+++ src/components/songCards/PlayingSongCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import SongSvg from "/assets/song.png";
+import previous from '/assets/previous.svg'
+import forward from '/assets/forward.svg'
+import next from '/assets/next.svg'
+import backward from '/assets/backward.svg'
+import Play from '/assets/video-circle.svg'
+
+function PlayingSongCard() {
+  return (
+    <div className=' w-[280px] h-[441px] flex flex-col border rounded-[16px] items-center justify-center '>
+        <div className=' rounded-[16px] mb-[20px] '>
+            <img className='rounded-[16px] ' width={248} height={213} src={SongSvg} alt="" />
+        </div>
+
+        <div className=' w-[248px] h-[99px]  flex flex-col items-center '>
+
+            <div id="songName " className=' font-sf-pro-display font-bold text-[18px] '>
+                Living My Best Life
+            </div>
+
+            <div id="artistName" className=' font-sf-pro-display font-medium text-[#99938F]   text-sm mb-[15px]'>
+                Ben Hector
+            </div>
+
+            <div id="seekBar" className=' flex flex-row  flex-wrap  ' >
+                <div id="seekline" className=' mx-3 w-[248px] h-[4px] bg-[#FFD7C2] '>
+                </div>
+                <div className=' w-full  flex justify-between text-[#99938F] font-sf-pro-display   text-[14px] mx-3 my-1 '>
+                <div id="songOnGoingTime">
+                     1:21
+                </div>
+                <div id="songLength">
+                    2:36
+                </div>
+                </div>
+            </div>
+
+        </div>
+        <div id="menuButtons " className=' flex items-center gap-[16px]' >
+            <button><img width={24} height={24} src={previous} alt="" /></button>
+            <button><img width={24} height={24} src={backward} alt="" /></button>
+            <button><img width={56} height={56} src={Play} alt="" /></button>
+            <button><img width={24} height={24} src={forward} alt="" /></button>
+            <button><img width={24} height={24} src={next} alt="" /></button>
+        </div>
+    </div>
+  )
+}
+
+export default PlayingSongCard
